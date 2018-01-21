ce3a85704990d840883c8ee35b433e4e6de731d9
Joe Johans <lgvichy@gmail.com>
2018-01-21 17:41:08 -0500

    Done with drawing 1

---
+++ week3/drawing-1/utils.js
@@ -0,0 +1,32 @@
+// create an arc for the context
+function makeArc(ctx, x, y, radius, start, end, ccw) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, start, end, ccw);
+    ctx.closePath();
+}
+
+// Fill an arc
+function fillArc(ctx, x, y, radius, start, end, ccw) {
+    makeArc(ctx, x, y, radius, start, end, ccw)
+    ctx.fill();
+}
+
+// Draw a circle
+function fillCircle(ctx, x, y, radius, ccw) {
+    fillArc(ctx, x, y, radius, 0, Math.PI * 2, ccw)
+}
+
+// Fill an equilateral triangle
+function fillEquilateralTriangle(ctx, size, center) {
+    const p1 = new Vector2(center.x, center.y - size);
+    const p2 = new Vector2(center.x - size, center.y + size);
+    const p3 = new Vector2(center.x + size, center.y + size);
+
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.lineTo(p3.x, p3.y);
+
+    ctx.closePath();
+    ctx.fill();
+}
